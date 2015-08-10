b53bd5b29f30b63977cbcc5c447fae916011286d
Sam Gentle <sam@samgentle.com>
2015-08-10 15:11:45 +1000

    add bandsjson, which writes bands atomically to a file

---
+++ bandsjson.py
@@ -0,0 +1,19 @@
+import os
+import json
+import gevent
+import zmq
+
+context = zmq.Context()
+
+bands = context.socket(zmq.SUB)
+bands.connect('ipc:///var/socks/bands')
+bands.setsockopt(zmq.SUBSCRIBE, b'')
+
+while True:
+    data = bands.recv()
+    with open('/tmp/data2.json', 'w') as f:
+        f.write(data)
+        f.flush()
+        os.fsync(f.fileno())
+    os.rename('/tmp/data2.json', '/tmp/data.json')
+
