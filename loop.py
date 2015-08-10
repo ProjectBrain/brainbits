dea8d1e675ad0db77ab45bd7aafb61c6546619dc
Sam Gentle <sam@samgentle.com>
2015-08-10 15:10:38 +1000

    add looper, like loader but repeats instead of stopping when the sample ends

---
+++ loop.py
@@ -0,0 +1,16 @@
+import zmq
+import sys
+import time
+import zmq
+
+filename = sys.argv[1] or 'dump'
+
+context = zmq.Context()
+raw = context.socket(zmq.PUB)
+raw.bind('ipc:///var/socks/raw')
+
+while True:
+  with open(filename, 'r') as f:
+    for line in f:
+      raw.send(line)
+      time.sleep(1./128)
