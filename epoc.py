3f58f481f4018bb4735e5d1b2ffc67b03047010b
Sam Gentle <sam@samgentle.com>
2019-09-03 16:46:22 +1000

    fix emokit probably

--- epoc.py
+++ epoc.py
@@ -1,8 +1,9 @@
 from emokit.emotiv import Emotiv
+from emokit.packet import EmotivExtraPacket
 import zmq
 import time
 
-headset = Emotiv()
+headset = Emotiv(display_output=False, verbose=True, force_old_crypto=False, is_research=False)
 
 context = zmq.Context()
 raw = context.socket(zmq.PUB)
@@ -15,14 +16,17 @@ def setup():
 def mainloop():
     while True:
         packet = headset.dequeue()
-        if packet is not None:
+        if packet is not None and type(packet) != EmotivExtraPacket:
             BAD_SENSORS = set(['X', 'Y', 'Z', 'Unknown'])
             values = {name: packet.sensors[name]['value'] for name in packet.sensors if name not in BAD_SENSORS}
             qualities = {name: packet.sensors[name]['quality']/8192 for name in packet.sensors if name not in BAD_SENSORS}
             #print(values)
             raw.send_json(values)
             quality.send_json(qualities)
-    time.sleep(0.001)
+        if not headset.running:
+            break
+        if packet is None:
+            time.sleep(0.005)
 
 def cleanup():
     raw.close(linger=0)
